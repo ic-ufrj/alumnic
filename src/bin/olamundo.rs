5e7688a5c7ddfb89159543de929a94b31eebbfdd
Arthur Bacci <arthurbacci@protonmail.com>
2025-07-05 21:22:25 -0300

    feat: commit inicial

---
+++ src/bin/olamundo.rs
@@ -0,0 +1,6 @@
+use alumnic::ola_mundo;
+
+fn main() {
+    println!("{}", ola_mundo());
+}
+
---
+++ src/lib.rs
@@ -0,0 +1,5 @@
+
+pub fn ola_mundo() -> String {
+    "Instituto de Computação / UFRJ".to_string()
+}
+
